dde099ce2a29e580da59cf7fb33db32fa8dc8048
Airidas Juska <airidas665@gmail.com>
2023-03-06 20:08:24 +0200

    upd

--- src/DeleteModal.jsx
+++
@@ -1,5 +0,0 @@
-function DeleteModal() {
-  return <div className='modal insufficient'>Account removed</div>;
-}
-
-export default DeleteModal;
--- src/DeletedModal.jsx
+++
@@ -1,9 +0,0 @@
-function DeletedModal({person}) {
-        
-    return <div className='modal insufficient'>
-        <span>Deleted {person.name} {person.lastName}</span>
-        </div>
-  }
-  
-  export default DeletedModal;
-  
